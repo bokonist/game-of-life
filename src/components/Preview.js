91fc130ef335a6ce8f35d2715b53dcc30db048c3
bokonist <jayrm1995@gmail.com>
2021-05-20 21:25:29 +0530

    preview display done+ added  option configs

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,66 @@
+import { useState } from "react";
+import "../styles/Preview.css";
 function Preview() {
+  let options = {
+    block: [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0],
+    ],
+    beeHive: [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 1, 0, 0, 1, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ],
+    loaf: [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 0, 0],
+      [0, 1, 0, 0, 1, 0],
+      [0, 0, 1, 0, 1, 0],
+      [0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ],
+  };
+  let [previewGrid, setPreviewGrid] = useState(options.beeHive);
+
   return (
     <div className="preview-container">
       <div className="preview-grid-container">
-        <button className="left-button"></button>
-        <div className="preview-grid"></div>
-        <button className="right-buttton"></button>
+        <button className="left-button carousel-button">{`<`}</button>
+        <div
+          className="preview-grid"
+          style={{
+            gridTemplateColumns: `repeat(${previewGrid[0].length}, 1fr)`,
+            gridTemplateRows: `repeat(${previewGrid.length}, 1fr)`,
+          }}
+        >
+          {previewGrid.map((rows, i) =>
+            rows.map((cell, j) => {
+              return (
+                <div
+                  key={`${i}-${j}`}
+                  style={{
+                    backgroundColor:
+                      previewGrid[i][j] === 1 ? "purple" : "transparent",
+                    height: `calc(20/${previewGrid.length})rem`,
+                  }}
+                  className="preview-grid-cell"
+                >
+                  {" "}
+                </div>
+              );
+            })
+          )}
+        </div>
+        <button className="right-button  carousel-button">{`>`}</button>
       </div>
-      <div className="preview-info"></div>
+      <div className="preview-info">
+        <p className="organism-name">Name: Block</p>
+        <p className="organism-classification">Classification: Still-life</p>
+      </div>
     </div>
   );
 }
