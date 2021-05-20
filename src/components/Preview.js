9721dc4903dd48e89805400a91c722746d138057
bokonist <jayrm1995@gmail.com>
2021-05-20 22:22:41 +0530

    finished carousel logic + added new options

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,35 +1,138 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/Preview.css";
 function Preview() {
-  let options = {
-    block: [
-      [0, 0, 0, 0],
-      [0, 1, 1, 0],
-      [0, 1, 1, 0],
-      [0, 0, 0, 0],
-    ],
-    beeHive: [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 1, 1, 0, 0],
-      [0, 1, 0, 0, 1, 0],
-      [0, 0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-    ],
-    loaf: [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 1, 1, 0, 0],
-      [0, 1, 0, 0, 1, 0],
-      [0, 0, 1, 0, 1, 0],
-      [0, 0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-    ],
-  };
-  let [previewGrid, setPreviewGrid] = useState(options.beeHive);
+  let organisms = [
+    {
+      name: "block",
+      startState: [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+      ],
+      classification: "Still Life",
+    },
+    {
+      name: "bee hive",
+      startState: [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ],
+      classification: "Still Life",
+    },
+    {
+      name: "loaf",
+      startState: [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ],
+      classification: "Still Life",
+    },
+    {
+      name: "boat",
+      startState: [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      classification: "Still Life",
+    },
+    {
+      name: "tub",
+      startState: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      classification: "Still Life",
+    },
+    {
+      name: "blinker",
+      startState: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      classification: "Oscillator",
+    },
 
+    {
+      name: "toad",
+      startState: [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ],
+      classification: "Oscillator",
+    },
+    {
+      name: "beacon",
+      startState: [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+      ],
+      classification: "Oscillator",
+    },
+    /*{
+      name: "placeholder",
+      startState: [],
+      classification: "",
+    },*/
+  ];
+  let [selection, setSelection] = useState(7);
+  let [previewGrid, setPreviewGrid] = useState(organisms[selection].startState);
+  //useEffect(() => {}, [previewGrid]);
+  const changeSelection = (direction) => {
+    let newSelection = 0;
+    if (direction === "left") {
+      setSelection((prevSelection) => {
+        newSelection = prevSelection - 1;
+        if (newSelection < 0) {
+          newSelection = organisms.length - 1;
+        }
+        setPreviewGrid(organisms[newSelection].startState);
+        return newSelection;
+      });
+    } else if (direction === "right") {
+      setSelection((prevSelection) => {
+        newSelection = prevSelection + 1;
+        if (newSelection >= organisms.length) {
+          newSelection = 0;
+        }
+        setPreviewGrid(organisms[newSelection].startState);
+        return newSelection;
+      });
+    }
+  };
   return (
     <div className="preview-container">
       <div className="preview-grid-container">
-        <button className="left-button carousel-button">{`<`}</button>
+        <button
+          className="left-button carousel-button"
+          onClick={() => {
+            changeSelection("left");
+          }}
+        >{`<`}</button>
         <div
           className="preview-grid"
           style={{
@@ -55,11 +158,18 @@ function Preview() {
             })
           )}
         </div>
-        <button className="right-button  carousel-button">{`>`}</button>
+        <button
+          className="right-button  carousel-button"
+          onClick={() => {
+            changeSelection("right");
+          }}
+        >{`>`}</button>
       </div>
       <div className="preview-info">
-        <p className="organism-name">Name: Block</p>
-        <p className="organism-classification">Classification: Still-life</p>
+        <p className="organism-name">Name: {organisms[selection].name}</p>
+        <p className="organism-classification">
+          Classification: {organisms[selection].classification}
+        </p>
       </div>
     </div>
   );
